16cdb14f57dfaa3de9243ab3a0ca28521db1048b
farooqcodeteck <160619047+farooqcodeteck@users.noreply.github.com>
2024-02-29 13:41:38 +0000

    Thu, Feb 29, 2024, 7:11 PM +05:30

--- one.c
+++ one.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 void meow (int n);
+
 int main (void)
 {
     meow (5);
 
 }
 
-void meow (int n)
+void meow   (int n)
 {
-    for (int  = 0; i < n; 1++)
+    for (int 1 = 0; i < n; 1++)
 {
     printf('meow \n");
 }
